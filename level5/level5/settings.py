2ecaf9ff40634036942b957753b97e618fdd2e93
nostreet <32536657+nostreet@users.noreply.github.com>
2018-01-10 19:58:13 +1300

    Update settings.py

--- level5/level5/settings.py
+++ level5/level5/settings.py
@@ -88,7 +88,6 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
